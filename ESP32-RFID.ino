eb3ca941dae028f78807f086f97051c1989f695b
Guilherme Dall'Agnol Deconto <deconto100@gmail.com>
2023-03-15 23:45:22 -0300

    chore: esp32 calling api

--- ESP32-RFID.ino
+++ ESP32-RFID.ino
@@ -1,9 +1,14 @@
 //Libraries
 #include <SPI.h> //https://www.arduino.cc/en/reference/SPI
 #include <MFRC522.h> //https://github.com/miguelbalboa/rfid
+#include <WiFi.h>
+#include <HTTPClient.h>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
+
+#include "WiFiCredentials.h"
 
 //Constants
 #define SS_PIN 5
@@ -15,82 +20,118 @@ using namespace std;
 
 // function declaration
 vector<int> getRFIDCode(byte *buffer, byte bufferSize);
+bool postClockInOut(string serverName, string tagid);
 
 //Variables
 MFRC522 rfid = MFRC522(SS_PIN, RST_PIN);
-vector<vector<int>> authorizedKeys {{204, 207, 8 , 57}, {179, 64, 12, 170}};
+string serverUrl = "http://192.168.0.228:3000/users/working/";
 
 void setup() {
-  //Init Serial USB
-  Serial.begin(115200);
-  Serial.println("Initialize System");
+    //Init Serial USB
+    Serial.begin(115200);
+    Serial.println("Initialize System");
 
-  SPI.begin();
-  rfid.PCD_Init();
+    // Init WiFi lib and connect to network
+    WiFi.begin(SSID, PASSWORD);
+    Serial.println("Connecting to wifi");
+    while(WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    
+    Serial.println("");
+    Serial.println("Connected to WiFi network with IP Address: ");
+    Serial.println(WiFi.localIP());
+    
+    SPI.begin();
+    rfid.PCD_Init();
 
-  pinMode(LED_BLUE, OUTPUT);
-  pinMode(LED_RED, OUTPUT);
+    pinMode(LED_BLUE, OUTPUT);
+    pinMode(LED_RED, OUTPUT);
 
-  Serial.print("Reader :");
-  rfid.PCD_DumpVersionToSerial();
+    Serial.print("Reader :");
+    rfid.PCD_DumpVersionToSerial();
 }
 
 void loop() {
-  readRFID();
+    readRFID();
 }
 
 //Read RFID card function
 void readRFID() {
-  
-  // Look for new card and verify if the NUID has been readed
-  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
-    return;
-  }
 
-  vector<int> currentRFID = getRFIDCode(rfid.uid.uidByte, rfid.uid.size);
+    // Look for new card and verify if the NUID has been readed
+    if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
+        return;
+    }
 
-  // Check if RFID has access
-  checkRFIDCode(currentRFID);
+    vector<int> currentRFID = getRFIDCode(rfid.uid.uidByte, rfid.uid.size);
 
-  // Stop reading PICC
-  rfid.PICC_HaltA();
+    // Check if RFID has access
+    checkRFIDCode(currentRFID);
 
-  // Stop encryption on PCD
-  rfid.PCD_StopCrypto1();
+    // Stop reading PICC
+    rfid.PICC_HaltA();
+
+    // Stop encryption on PCD
+    rfid.PCD_StopCrypto1();
 
 }
 
 void checkRFIDCode(vector<int> currentRFID) {
-  bool isAuthorized = false;
-  for (vector<int> key : authorizedKeys) {
-    if (currentRFID == key) {
-      isAuthorized = true;
-      break;
+    stringstream ss;
+    for (auto it = currentRFID.begin(); it != currentRFID.end(); it++)    {
+        ss << *it;
     }
-  }
-  Serial.println(isAuthorized ? "RIFD is authorized!": "RIFD is not authorized!");
-  letUserIn(isAuthorized);
+    bool isAuthorized = postClockInOut(ss.str());
+    letUserIn(isAuthorized);
 }
 
 void letUserIn(bool isAuthorized) {
-  if (isAuthorized) {
-      digitalWrite(LED_BLUE, HIGH);
-      delay(2000);
-      digitalWrite(LED_BLUE, LOW);
-  } else {
-      digitalWrite(LED_RED, HIGH); 
-      delay(2000);
-      digitalWrite(LED_RED, LOW);
-  }
+    if (isAuthorized) {
+        digitalWrite(LED_BLUE, HIGH);
+        delay(2000);
+        digitalWrite(LED_BLUE, LOW);
+    } else {
+        digitalWrite(LED_RED, HIGH);
+        delay(2000);
+        digitalWrite(LED_RED, LOW);
+    }
+}
+
+bool postClockInOut(string tagid) {
+    if(WiFi.status() == WL_CONNECTED) {
+        WiFiClient client;
+        HTTPClient http;
+
+        string url = serverUrl + tagid;
+        http.begin(client, url.c_str());
+        Serial.println(url.c_str());
+        http.addHeader("Content-Type", "application/json");
+
+        int httpResponseCode = http.POST("");
+
+        // Free resources
+        http.end();
+
+        Serial.println(httpResponseCode);
+
+        if (httpResponseCode >= 200 && httpResponseCode <= 302) {
+            return true;
+        }
+    } else {
+        Serial.println("WiFi not connected!");
+    }
+    return false;
 }
 
 vector<int> getRFIDCode(byte *buffer, byte bufferSize) {
-  vector<int> currentRFID;
-  Serial.print("RFID code: ");
-  for (byte i = 0; i < bufferSize; i++) {
-    currentRFID.push_back(buffer[i]);
-    Serial.print(buffer[i]);
-  }
-  Serial.println("");
-  return currentRFID;
+    vector<int> currentRFID;
+    Serial.print("RFID code: ");
+    for (byte i = 0; i < bufferSize; i++) {
+        currentRFID.push_back(buffer[i]);
+        Serial.print(buffer[i]);
+    }
+    Serial.println("");
+    return currentRFID;
 }
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # ESP32-RFID
+
+## Description
+
+This project aims to create a simple RFID reader using an ESP32 and a MFRC522 RFID reader. The RFID reader will be able to read the UID of a card and send it to a NestJS server. The backend server will then be able to allow a certain user to do something.
+
+For this system to work there are a few important components:
+- Tags or ID Cards: All users have an ID card or Tag which is identified using UID. The UID is a 4 byte number which is unique to each card. The UID is read by the RFID reader and sent to the server.
+- ESP32: The ESP32 is a microcontroller which is able to read the UID of the card and send it to the server.
+- Backend: The backend is a NestJS server which is able to receive the UID and check if the user is allowed to do something. This backend is connected to a relational database PostgreSQL using TypeORM as ORM.
+
+## Deploy
+
+During the process of creating a software application, it's common to write code within a development environment. However, at some point, it becomes necessary to launch the application in a production environment while still carrying out the development process within the development environment. For this reason, it's important to be able to manage environment variables in a way that allows us to easily switch between environments. In this project, we use the the NestJS-Config module.
+
+Afterwards, we will use Docker's containers to deploy our code by producing an image using docker-compose and our code.
+
+### Circuit Schematic
+
+Bellow you can find the schematic of the circuit.
+
+![Schematic](./assets/schematic.png)
+
+## Essentials
+
+Create a file called WiFiCredentials.h in the root folder and add your wifi credentials like so:
+
+```c++
+#define SSID "SSID"
+#define PASSWORD "PASSWORD"
+```
